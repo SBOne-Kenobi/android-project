ad65b9f9d79b3b8d81d00fa034ad044516991497
Rustam Sadykov <sbonekenobi@gmail.com>
2021-10-13 16:16:56 +0300

    add view pager

--- app/src/main/java/com/rustamsadykov/firstapp/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/rustamsadykov/firstapp/ui/onboarding/OnboardingFragment.kt
@@ -3,11 +3,15 @@ package com.rustamsadykov.firstapp.ui.onboarding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 import com.rustamsadykov.firstapp.R
 import com.rustamsadykov.firstapp.databinding.FragmentOnboardingBinding
 
@@ -17,14 +21,20 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     private var player: ExoPlayer? = null
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         player = SimpleExoPlayer.Builder(requireContext()).build().apply {
             addMediaItem(MediaItem.fromUri("asset:///onboarding.mp4"))
             repeatMode = Player.REPEAT_MODE_ALL
             prepare()
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewBinding.playerView.player = player
+        viewBinding.onboardingViewPager.setTextPages()
+        viewBinding.onboardingViewPager.attachDots(viewBinding.onboardingTextTabLayout)
     }
 
     override fun onResume() {
@@ -40,6 +50,22 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
     override fun onDestroy() {
         super.onDestroy()
         player?.release()
+    }
+
+    private fun ViewPager2.setTextPages() {
+        adapter =
+            ListDelegationAdapter(onboardingTextAdapterDelegate()).apply {
+                items =
+                    listOf(
+                        getString(R.string.onboarding_view_pager_text_1),
+                        getString(R.string.onboarding_view_pager_text_2),
+                        getString(R.string.onboarding_view_pager_text_3)
+                    )
+            }
+    }
+
+    private fun ViewPager2.attachDots(tabLayout: TabLayout) {
+        TabLayoutMediator(tabLayout, this) { _, _ -> }.attach()
     }
 
 }
