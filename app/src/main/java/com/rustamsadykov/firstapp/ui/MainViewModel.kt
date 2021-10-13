93b84651832997c5b74a94d151f729a14dc77893
Rustam Sadykov <sbonekenobi@gmail.com>
2021-10-13 23:07:50 +0300

    add navigation graphs

--- app/src/main/java/com/rustamsadykov/firstapp/ui/MainViewModel.kt
+++ app/src/main/java/com/rustamsadykov/firstapp/ui/MainViewModel.kt
@@ -1,5 +1,16 @@
 package com.rustamsadykov.firstapp.ui
 
+import androidx.lifecycle.viewModelScope
 import com.rustamsadykov.firstapp.ui.base.BaseViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class MainViewModel : BaseViewModel()
+class MainViewModel : BaseViewModel() {
+
+    private val _isAuthorizedFlow: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isAuthorizedFlow: Flow<Boolean>
+        get() = _isAuthorizedFlow.asStateFlow()
+
+}
