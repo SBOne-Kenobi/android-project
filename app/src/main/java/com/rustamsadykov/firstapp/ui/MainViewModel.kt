6400ff58f46814fc037b5d5016cb2964df320ab4
Rustam Sadykov <sbonekenobi@gmail.com>
2021-10-27 11:36:39 +0300

    add land for onboarding

--- app/src/main/java/com/rustamsadykov/firstapp/ui/MainViewModel.kt
+++ app/src/main/java/com/rustamsadykov/firstapp/ui/MainViewModel.kt
@@ -1,11 +1,9 @@
 package com.rustamsadykov.firstapp.ui
 
-import androidx.lifecycle.viewModelScope
 import com.rustamsadykov.firstapp.ui.base.BaseViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 class MainViewModel : BaseViewModel() {
 
