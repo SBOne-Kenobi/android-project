4cd9fc9436a118f8ca08fa865038819ca9940a78
Rustam Sadykov <sbonekenobi@gmail.com>
2021-12-17 16:17:29 +0300

    add logout

--- app/src/main/java/com/rustamsadykov/firstapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/rustamsadykov/firstapp/ui/profile/ProfileFragment.kt
@@ -1,14 +1,56 @@
 package com.rustamsadykov.firstapp.ui.profile
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rustamsadykov.firstapp.R
 import com.rustamsadykov.firstapp.databinding.FragmentProfileBinding
 import com.rustamsadykov.firstapp.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.insetter.applyInsetter
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class ProfileFragment : BaseFragment(R.layout.fragment_profile) {
 
     private val viewBinding by viewBinding(FragmentProfileBinding::bind)
 
     private val viewModel: ProfileViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToEvents()
+        viewBinding.logoutButton.applyInsetter {
+            type(statusBars = true) { margin() }
+        }
+        viewBinding.logoutButton.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
+    private fun subscribeToEvents() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventsFlow().collect { event ->
+                    when (event) {
+                        is ProfileViewModel.Event.LogoutError -> {
+                            Toast
+                                .makeText(
+                                    requireContext(),
+                                    R.string.common_general_error_text,
+                                    Toast.LENGTH_LONG
+                                )
+                                .show()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
